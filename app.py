9c23945fe7293d1be6c468f36534addcbb0514a4
RhysIsDaBestForSure <164345947+RhysIsDaBestForSure@users.noreply.github.com>
2024-05-10 08:13:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,56 +20,35 @@ lemmatizer = WordNetLemmatizer()
 
 clf = load('model.joblib')
 vectorizer = load('vectorizer.joblib')
+
 def preprocess_text(text):
     try:
-        # Lowercase the text
         text = text.lower()
-    
-        # Remove punctuation and digits
         text = text.translate(str.maketrans('', '', string.punctuation + string.digits))
-    
-        # Remove the specific word "Reuters"
-        text = text.replace("reuters", "")
-        text = text.replace("Reuters", "")
-    
-    
-        # Tokenize the text
+        text = text.replace("reuters", "").replace("Reuters", "")
         words = word_tokenize(text)
-    
-        # Remove stop words
         words = [word for word in words if word not in stop_words]
-    
-        # Stem or lemmatize the words
         words = [stemmer.stem(word) for word in words]
-       
-            # Join the words back into a string
         text = ' '.join(words)
-    
         return text
     except:
         abort(404)
-
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    user_query = ''  # Initialize user_query here
     if request.method == 'POST':
         user_query = request.form['query']
         
-        # Get top headlines based on user query
-        top_headlines = newsapi.get_top_headlines(q=user_query,
-                                                  language='en',
-                                                  country='us')
+        top_headlines = newsapi.get_top_headlines(q=user_query, language='en', country='us')
         top_headlines = top_headlines['articles'][:10]
 
-        # Get all articles based on user query
-        all_articles = newsapi.get_everything(q=user_query,
-                                              language='en',
-                                              sort_by='relevancy')
-        
+        all_articles = newsapi.get_everything(q=user_query, language='en', sort_by='relevancy')
         all_articles = all_articles['articles'][:10]
     return render_template('home.html', user_query=user_query, top_headlines=top_headlines, all_articles=all_articles)
+
 @app.errorhandler(404)
 def page_not_found(error):
     error="Hmm. I don't know what you did but it broke something. Did you try and input a carrot again?"
@@ -79,28 +58,22 @@ def page_not_found(error):
 def predict():
     try: 
         url = request.form['text']
-        # download and parse article
         html = urlopen(url).read()
         soup = BeautifulSoup(html, features="html.parser")
     
-        # Remove script and style elements
         for script in soup(["script", "style"]):
             script.extract()
     
-        # Remove specific elements by tag name
         for tag in soup.find_all(["nav", "footer", "header", "aside"]):
             tag.extract()
     
-        # Get text
         text = soup.get_text(separator='\n', strip=True)
     
-        # Remove specific unwanted text patterns
         unwanted_texts = ["Do you want to enable cookies", "this video cannot be played","BBC News Image source","Getty Images"," EPA Image caption"]
         for unwanted_text in unwanted_texts:
             if unwanted_text in text:
                 text = text.replace(unwanted_text, '')
     
-        print(text)
         preprocessed_text = preprocess_text(text)
         X = vectorizer.transform([preprocessed_text])
         y_pred = clf.predict(X)
@@ -108,14 +81,12 @@ def predict():
             result = 'Real'
         else:
             result = 'Fake'
-        # Analyze the sentiment of the article using the VADER sentiment analyzer
         analyzer = SentimentIntensityAnalyzer()
         sentiment_dict = analyzer.polarity_scores(text)
         negative = sentiment_dict['neg']* 100
         neutral = sentiment_dict['neu']*100
         positive = sentiment_dict['pos']*100
     
-        # decide sentiment as positive, negative and neutral
         if sentiment_dict['compound'] >= 0.05 :
             compound = "Positive"
         elif sentiment_dict['compound'] <= - 0.05 :
